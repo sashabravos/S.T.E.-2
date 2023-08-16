8bb5c890aba6a199cfe93881c0bf9bf80bad6638
Alexandra Kostrova <sashabravos@gmail.com>
2023-08-16 23:37:06 +0300

    Add folders, create TabBarController, add empty ViewControllers, Constants file and Country Images to Assets

---
+++ TableViewTask/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  TableViewTask
+//
+//  Created by Александра Кострова on 14.08.2023.
+//
+
+import UIKit
+
+enum Constants {
+    
+    enum Lists {
+        // Countries
+        static let countryNames = ["Austria", "Belgium", "France", "Germany", "Greece"]
+    }
+}
+
+
---
+++ TableViewTask/ViewControllers/Task1/TaskOneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TaskOneViewController.swift
+//  TableViewTask
+//
+//  Created by Александра Кострова on 14.08.2023.
+//
+
+import UIKit
+
+class TaskOneViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
